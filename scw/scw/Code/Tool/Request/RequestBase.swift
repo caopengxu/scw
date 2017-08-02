df7f4eac8987e8b356921808df95e1823a6fa356
caopengxu <762095508@qq.com>
2017-08-02 14:53:27 +0800

    更新pods

---
+++ scw/scw/Code/Tool/Request/RequestBase.swift
@@ -0,0 +1,17 @@
+//
+//  RequestBase.swift
+//  scw
+//
+//  Created by cpx on 2017/8/1.
+//  Copyright © 2017年 scw. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class RequestBase: NSObject {
+    
+    
+    
+    
+}
