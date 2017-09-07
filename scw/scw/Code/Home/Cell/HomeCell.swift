e97b6e1c5f7108e713d483596f443abef1f2a7e9
caopengxu <762095508@qq.com>
2017-09-07 11:21:10 +0800

    首页界面

--- scw/scw/Code/Home/Cell/HomeCell.swift
+++ scw/scw/Code/Home/Cell/HomeCell.swift
@@ -1,25 +1,24 @@
 //
-//  GoodsCell.swift
+//  HomeCell.swift
 //  scw
 //
-//  Created by cpx on 2017/7/21.
+//  Created by cpx on 2017/9/5.
 //  Copyright © 2017年 scw. All rights reserved.
 //
 
 import UIKit
 
-class GoodsCell: UITableViewCell {
+class HomeCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        
+        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        
+        // Configure the view for the selected state
     }
     
 }
--- scw/scw/Code/Home/Controller/HomeController.swift
+++ scw/scw/Code/Home/Controller/HomeController.swift
@@ -10,25 +10,69 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 1000.0
+        tableView.register(UINib.init(nibName: "HomeCell", bundle: nil), forCellReuseIdentifier: "HomeCell")
         
     }
 
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+// MARK: - 表格数据源方法
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 2;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let nib = UINib.init(nibName: "ClassHeaderView", bundle: Bundle.main)
+        let view = nib.instantiate(withOwner: nib, options: nil)[0] as! ClassHeaderView
+        
+        return view
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
+            
+            return cell;
+        }
+        else
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell") as! HomeCell
+            
+            
+            return cell;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("点击了")
+    }
+}
+
+
